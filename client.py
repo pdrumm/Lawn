c5f97af760bfc2c69c184fee9139e47ba0ab919c
Kris Thieneman <kthienem@nd.edu>
2016-04-30 18:58:21 -0400

    add multiplayer functionality

--- client.py
+++ client.py
@@ -26,47 +26,57 @@ class GameSpace(object):
 		self.size = self.width, self.height = (640, 480)
 		self.black = (0, 0, 0)
 		self.screen = pygame.display.set_mode(self.size)
-		self.square = Square("temp.png", [-100, -100], self)
+		# self.square = Square("temp.png", [-100, -100], self)
 		#self.shadow = Square("laser.png",self)
-		self.shadow = pygame.sprite.Group()
+		self.player_mowers = []
+		self.player_shadows = []
+		# self.shadow = pygame.sprite.Group()
 		self.curr_shadow = None
 		receive.get().addCallback(self.receiveCallback)
 		# tick regulation variable
 		self.tick = 0
 		self.flip_rate = 30
 		self.dir = 0
+		self.ready = False
+		self.num_players = 0
 
 	def main(self):
-		self.tick = (self.tick+1)%self.flip_rate
-		#user input
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				reactor.stop()
-			elif event.type == KEYDOWN:
-				if event.key == K_UP:
-					send.put("up")
-					self.dir = 1
-				elif event.key == K_DOWN:
-					send.put("down")
-					self.dir = 3
-				elif event.key == K_LEFT:
-					send.put("left")
-					self.dir = 2
-				elif event.key == K_RIGHT:
-					send.put("right")
-					self.dir = 0
+		if not self.ready:
+			self.screen.fill((255, 0, 0))
+		else:
+			self.tick = (self.tick+1)%self.flip_rate
+			#user input
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					reactor.stop()
+				elif event.type == KEYDOWN:
+					if event.key == K_UP:
+						send.put("up")
+						self.dir = 1
+					elif event.key == K_DOWN:
+						send.put("down")
+						self.dir = 3
+					elif event.key == K_LEFT:
+						send.put("left")
+						self.dir = 2
+					elif event.key == K_RIGHT:
+						send.put("right")
+						self.dir = 0
 
-		#display game objects
-		self.screen.fill(self.black)
-		# if len(self.shadow.sprites()) > 0:
-			# self.screen.blit(self.curr_shadow.image, self.curr_shadow.rect)
-		# self.screen.blit(self.square.image, self.square.rect)
-#		print '({x},{y})'.format(x=self.square.rect.x,y=self.square.rect.y)
+			#display game objects
+			self.screen.fill(self.black)
+			if len(self.player_shadows > 0):
+			# if len(self.shadow.sprites()) > 0:
+				# self.screen.blit(self.curr_shadow.image, self.curr_shadow.rect)
+			# self.screen.blit(self.square.image, self.square.rect)
+	#		print '({x},{y})'.format(x=self.square.rect.x,y=self.square.rect.y)
 
-		# update does not have the overhead of flip b/c it only blits the args, not the entire page
-		# if self.tick == 0:
-		self.shadow.draw(self.screen)
-		self.screen.blit(self.square.image, self.square.rect)
+			# update does not have the overhead of flip b/c it only blits the args, not the entire page
+			# if self.tick == 0:
+				for player in self.player_shadows:
+					player.draw(self.screen)
+				for player in self.player_mowers:
+					self.screen.blit(player.image, player.rect)
 		pygame.display.flip()
 		# else:
 			# pygame.display.update(self.square)
@@ -75,13 +85,21 @@ class GameSpace(object):
 		#receive new center
 		center = pickle.loads(data)
 		#create new square sprite with that center
-		self.curr_shadow = Square("laser_original.png", [center[0], center[1]], self)
+		for i in xrange(self.num_players):
+			self.curr_shadow = Square("laser_original.png", [center[0], center[1]], self)
 		#update center of player
-		self.square.rect.center = [center[0], center[1]]
-		self.square.image = self.square.rot_center(self.square.original_image, 90*self.dir)
+			self.player_mowers[i].rect.center = [center[i][0], center[i][1]]
+			self.player_mowers[i].image = self.square.rot_center(self.square.original_image, 90*self.dir)
 		#add new sprite to group
-		self.shadow.add(self.curr_shadow)
+			self.player_shadows[i].add(self.curr_shadow)
 		receive.get().addCallback(self.receiveCallback)
+
+	def make_players(self):
+		for i in xrange(self.num_players):
+			self.player_mowers.append(Square("temp.png", [-100, -100], self))
+			self.player_shadows.append(pygame.sprite.Group())
+
+		self.ready = True
 
 class Square(pygame.sprite.Sprite):
 	def __init__(self, img_file, center, gs=None):
@@ -114,7 +132,13 @@ class ServerConn(Protocol):
 		print "connection lost to", SERVER_HOST, "port", SERVER_PORT
 
 	def dataReceived(self, data):
-		receive.put(data)
+		if not self.gs.ready:
+			unpack = pickle.loads(data)
+			for key in unpack.keys():
+				self.gs.num_players = unpack[key]
+				self.gs.make_players()
+		else:
+			receive.put(data)
 
 	def sendCallback(self, data):
 		self.transport.write(data)
